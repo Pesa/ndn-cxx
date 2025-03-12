fb0aca1b6d55f94914bdf469029c30ed23a1a482
Davide Pesavento <davidepesa@gmail.com>
2025-03-12 17:42:20 -0400

    ci: use pipx to install gcovr and sphinx
    Change-Id: Ie34066a85d56ff453f9b06ce7bf6f27cd448565e

--- .jenkins.d/00-deps.sh
+++ .jenkins.d/00-deps.sh
@@ -18,16 +18,15 @@ APT_PKGS=(
     python3
 )
 FORMULAE=(boost openssl pkgconf)
-PIP_PKGS=()
 case $JOB_NAME in
     *code-coverage)
-        APT_PKGS+=(lcov python3-pip)
-        PIP_PKGS+=('gcovr~=5.2')
+        APT_PKGS+=(lcov python3-venv)
+        PIPX=pipx
         ;;
     *Docs)
-        APT_PKGS+=(doxygen graphviz python3-pip)
-        FORMULAE+=(doxygen graphviz)
-        PIP_PKGS+=(sphinx sphinxcontrib-doxylink)
+        APT_PKGS+=(doxygen graphviz python3-venv)
+        FORMULAE+=(doxygen graphviz pipx)
+        PIPX=pipx
         ;;
 esac
 
@@ -43,11 +42,21 @@ if [[ $ID == macos ]]; then
 elif [[ $ID_LIKE == *debian* ]]; then
     sudo apt-get update -qq
     sudo apt-get install -qy --no-install-recommends "${APT_PKGS[@]}"
+    if [[ -n $PIPX && -z $GITHUB_ACTIONS ]]; then
+        curl -fLOSs --output-dir "$CACHE_DIR" 'https://github.com/pypa/pipx/releases/download/1.7.1/pipx.pyz'
+        PIPX="python3 $CACHE_DIR/pipx.pyz"
+    fi
 elif [[ $ID_LIKE == *fedora* ]]; then
     sudo dnf install -y gcc-c++ libasan lld pkgconf-pkg-config python3 \
                         boost-devel openssl-devel sqlite-devel
 fi
 
-if (( ${#PIP_PKGS[@]} )); then
-    pip3 install --user --upgrade --upgrade-strategy=eager "${PIP_PKGS[@]}"
-fi
+case $JOB_NAME in
+    *code-coverage)
+        $PIPX install 'gcovr~=5.2'
+        ;;
+    *Docs)
+        $PIPX install 'sphinx~=8.0'
+        $PIPX inject sphinx sphinxcontrib-doxylink
+        ;;
+esac
